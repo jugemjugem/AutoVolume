4c5bbc839e4e7c1350895ca67b54417f1f7e6b6f
ushizaki <ushizaki@gmail.com>
2024-03-15 08:11:24 +0900

    add mfc

---
+++ MicMuteMfc/MicMuteMfc.h
@@ -0,0 +1,32 @@
+﻿
+// MicMuteMfc.h : PROJECT_NAME アプリケーションのメイン ヘッダー ファイルです
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "PCH に対してこのファイルをインクルードする前に 'pch.h' をインクルードしてください"
+#endif
+
+#include "resource.h"		// メイン シンボル
+
+
+// CMicMuteMfcApp:
+// このクラスの実装については、MicMuteMfc.cpp を参照してください
+//
+
+class CMicMuteMfcApp : public CWinApp
+{
+public:
+	CMicMuteMfcApp();
+
+// オーバーライド
+public:
+	virtual BOOL InitInstance();
+
+// 実装
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CMicMuteMfcApp theApp;
